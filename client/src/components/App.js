c973a5ce975c82bf94465392a24ee8edffbabb85
Brandan Sayarath <brandan@brandan.tech>
2020-12-19 11:07:30 -0800

    scaffold stream components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,33 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      Page 1<Link to="/pagetwo">Go to page two</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      Page 2<br />
+      <button>Click me!</button>
+      <Link to="/">Go to page one</Link>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
