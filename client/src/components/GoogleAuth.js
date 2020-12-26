79cb3bbdc43b2c7e93a9dd8849695314aae688c9
Brandan Sayarath <brandan@brandan.tech>
2020-12-26 13:52:16 -0800

    add sign-in logic for Google oAuth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,35 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "170324696986-ucsjm542moffka1a56ibb1lfe7basff5.apps.googleusercontent.com",
-        scope: "",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "170324696986-ucsjm542moffka1a56ibb1lfe7basff5.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>UNKNOWN SIGNIN</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>SIGNED IN</div>;
+    } else {
+      return <div>SIGNED OUT</div>;
+    }
+  }
+
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
